44e416352d75fccbb2bf6dd68619e4a5fecc377c
KhalilAndolsi <114878060+KhalilAndolsi@users.noreply.github.com>
2024-02-13 23:20:59 +0000

    add timing function

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,6 +6,10 @@ import { generateEmailContent, sendEmail } from "@/lib/nodeMailer";
 import { scrapeAmazonProduct } from "@/lib/scrape";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET() {
   try {
     connectToDB();
