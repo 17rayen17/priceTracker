ff40fdef423dcd5138b837d761660a1183c03147
KhalilAndolsi <114878060+KhalilAndolsi@users.noreply.github.com>
2024-02-14 14:40:02 +0000

    maxDuration

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import { generateEmailContent, sendEmail } from "@/lib/nodeMailer";
 import { scrapeAmazonProduct } from "@/lib/scrape";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+// export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
